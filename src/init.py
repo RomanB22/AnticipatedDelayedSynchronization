d3b4051a54f094dc3731c394fd858dedafc358ea
Roman Baravalle <romanbaravalle@gmail.com>
2025-06-18 14:18:06 -0500

    nrnivmodl execution

--- src/init.py
+++ src/init.py
@@ -2,6 +2,10 @@ from netpyne import sim  # import netpyne init module
 
 # cfg, netParams = sim.loadFromIndexFile('index.npjson')
 # read cfg and netParams from command line arguments if available; otherwise use default
+
+# Mock up
+# if not exists('x86_64'):
+#     execute('nrnivmodl mod')
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='src/cfg.py', netParamsDefault='src/netParams.py')
 sim.createSimulateAnalyze(netParams = netParams, simConfig = cfg)  # create and simulate network
 
